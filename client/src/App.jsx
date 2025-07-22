0a788dad455f17a085e79f2e0695071ed163d743
Michau <botkotel@gmail.com>
2025-07-22 13:39:19 +0200

    Added routing for new views.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Views/Home.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home.jsx";
+import Login from "./views/Login.jsx";
+import NotFound from "./views/NotFound";
+import Dashboard from "./views/Dashboard.jsx";
+import Logout from "./views/Logout.jsx";
+import Analytics from "./views/Modules/Analytics.jsx";
+import Generator from "./views/Modules/PlaylistGenerator.jsx";
+import Manager from "./views/Modules/PlaylistManager.jsx";
+import Rec from "./views/Modules/Recommendations.jsx";
+import Search from "./views/Modules/Search.jsx";
 
-function App() {
-
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/modules/analytics" element={<Analytics />} />
+        <Route path="/modules/playlistgenerator" element={<Generator />} />
+        <Route path="/modules/playlistmanager" element={<Manager />} />
+        <Route path="/modules/recommendations" element={<Rec />} />
+        <Route path="/modules/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
-
-export default App
