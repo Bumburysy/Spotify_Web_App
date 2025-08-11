038f764eea5fdbb4ce325acd01be3cd0e5c85768
Michau <botkotel@gmail.com>
2025-08-11 15:11:24 +0200

    Implemented login with Spotify account.

---
+++ client/src/components/LoginWithSpotify.jsx
@@ -0,0 +1,27 @@
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+
+const SCOPE = encodeURIComponent("user-read-email user-read-private");
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "code";
+
+export default function LoginWithSpotify() {
+  const handleLogin = () => {
+    const loginURL =
+      `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}` +
+      `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+      `&response_type=${RESPONSE_TYPE}` +
+      `&scope=${SCOPE}` +
+      `&show_dialog=true`;
+
+    window.location.href = loginURL;
+  };
+
+  return (
+    <div>
+      <a onClick={handleLogin} role="button">
+        Zaloguj
+      </a>
+    </div>
+  );
+}
