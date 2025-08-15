cd75e174d769f5e766f394ca64329fe98b23b1e8
Michau <botkotel@gmail.com>
2025-08-15 16:22:30 +0200

    Added more SCOPEs to access user data from Spotify API.

--- client/src/components/LoginWithSpotify.jsx
+++ client/src/components/LoginWithSpotify.jsx
@@ -1,7 +1,25 @@
 const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
 
-const SCOPE = encodeURIComponent("user-read-email user-read-private");
+const SCOPE = encodeURIComponent(
+  [
+    "user-read-email",
+    "user-read-private",
+    "user-top-read",
+    "user-read-recently-played",
+    "user-read-playback-position",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "playlist-modify-private",
+    "playlist-modify-public",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "user-follow-read",
+  ].join(" ")
+);
+
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "code";
 
@@ -13,10 +31,8 @@ export default function LoginWithSpotify() {
       `&response_type=${RESPONSE_TYPE}` +
       `&scope=${SCOPE}` +
       `&show_dialog=true`;
-
     window.location.href = loginURL;
   };
-
   return (
     <div>
       <a onClick={handleLogin} role="button">
